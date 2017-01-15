10725611816209694f7d87d41c2a000fa241423d
backroot <kotaro@codebase.jp>
2017-01-15 22:50:30 +0900

    YUM設定追加

--- setup_centos_6.sh
+++ setup_centos_6.sh
@@ -8,13 +8,20 @@ echo "Setup starting."
 
 cat /etc/centos-release
 
-# Perl Install
+# Perl install
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
 yum -y install perl perl-core
+
+# Kernel devel install
+yum -y update kernel
+yum -y install kernel-devel kernel-headers gcc gcc-c++
 
 # Network setup
 IFCFG_ETH0=/etc/sysconfig/network-scripts/ifcfg-eth0
 perl -pi -e "s/^ONBOOT=no/ONBOOT=yes/i" $IFCFG_ETH0
+perl -pi -e "s/^HWADDR=.+\n//i" $IFCFG_ETH0
+perl -pi -e "s/^UUID=.+\n//i" $IFCFG_ETH0
+ln -fs /dev/null /etc/udev/rules.d/70-persistent-net.rules
 cat $IFCFG_ETH0
 service network restart
 ifconfig
@@ -27,7 +34,7 @@ echo "vagrant" | passwd vagrant --stdin
 # SSH setup
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
-curl -k -L -o /home/vagrant/.ssh/authorized_keys https://github.com/mitchellh/vagrant/blob/master/keys/vagrant.pub
+curl -k -L -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:wheel /home/vagrant/.ssh
 
@@ -48,7 +55,15 @@ chkconfig ip6tables off
 # yum setup
 yum -y install epel-release
 rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
-yum install http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+yum -y install http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+perl -pi -e "s/enabled=1/enabled=0/ig" /etc/yum.repos.d/epel.repo
+perl -pi -e "s/enabled=1/enabled=0/ig" /etc/yum.repos.d/remi-safe.repo
+yum clean all
 
+# Optimize box size
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
 
 echo "Setup finished."
+
+shutdown -r now
