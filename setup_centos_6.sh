afd4eb86c8b21d3bb64e4d891a3b099779e47881
backroot <kotaro@codebase.jp>
2017-01-15 17:11:20 +0900

    eth0のオンブートを有効化

--- setup_centos_6.sh
+++ setup_centos_6.sh
@@ -4,7 +4,15 @@ set -e
 
 echo "Setup starting."
 
-cat /etc/redhat-release
+cat /etc/centos-release
+
+# Perl Install
 yum -y install perl perl-core
+
+# Network Configuration
+perl -pi -e "s/^ONBOOT=no/ONBOOT=yes/i" /etc/sysconfig/network-scripts/ifcfg-eth0
+service network restart
+ifconfig
+
 
 echo "Setup finished."
